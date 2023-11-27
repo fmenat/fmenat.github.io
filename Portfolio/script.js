f204bfbb4278e947e2bb41e5f49bd253bddac980
Francisco Mena <famenatoro@gmail.com>
2023-11-27 19:21:33 +0100

    format correction

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -58,7 +58,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .profile-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p1, .about-content', { origin: 'right' });
 
